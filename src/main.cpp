7b6073c90d50f5bfad37505e9812a1c07b79f31a
orion160 <idbl64@outlook.com>
2023-06-13 23:16:20 -0500

    Hello Vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <stdexcept>
+#include <vulkan/vulkan.hpp>
 
 int main(int argc, char **argv)
 {
-    std::cout << "Hello World!!!\n";
+    auto instanceVersion{vk::enumerateInstanceVersion()};
+
+    std::cout << "Vulkan instance version v" << VK_API_VERSION_MAJOR(instanceVersion) << '.'
+              << VK_API_VERSION_MINOR(instanceVersion) << '.' << VK_API_VERSION_PATCH(instanceVersion) << " - "
+              << "variant " << VK_API_VERSION_VARIANT(instanceVersion) << '\n';
+
+    vk::ApplicationInfo appInfo{"Hello Vulkan", VK_MAKE_API_VERSION(0, 0, 1, 0), "Ophiucus", VK_API_VERSION_1_3};
+
+    vk::InstanceCreateInfo instanceInfo{{}, &appInfo, {}, {}, nullptr};
+
+    auto instance = vk::createInstance(instanceInfo);
+
+    instance.destroy();
+
+    std::cout << "Hello Vulkan!!!\n";
 
     return 0;
 }
